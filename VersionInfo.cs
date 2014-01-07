40c90ac2ff8c090cda00a50fc6962974afa8b5f2
Steve Hansen <steve@2sky.be>
2014-01-07 11:41:36 +0100

    Fixed NuGet issues

---
+++ VersionInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("2sky NV")]
+[assembly: AssemblyCopyright("Copyright © 2sky NV 2010-2013")]
+[assembly: AssemblyTrademark("")]
+
+[assembly: AssemblyVersion("4.5.05900.2948")]
+[assembly: AssemblyFileVersion("4.5.05900.2948")]
+
+[assembly: ComVisible(false)]
